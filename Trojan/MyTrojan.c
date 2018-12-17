493bc0b8953021a6acf2fb29b50073dc4b44e8a8
Matan Oded <matanoded@gmail.com>
2018-12-17 09:56:47 +0200

    .

--- Trojan/MyTrojan.c
+++ Trojan/MyTrojan.c
@@ -4,8 +4,10 @@
 int main(int argc, char **agrv)
 {
 	char buff[100];
-	HANDLE h = OpenProcess(PROCESS_ALL_ACCESS, FALSE, 2308);
+	HANDLE h = OpenProcess(PROCESS_ALL_ACCESS, FALSE, 9492);
 	int length = GetModuleFileNameExA(h, NULL, buff, strlen(buff));
+
+	LPVOID mem = VirtualAllocEx(h, NULL, 100, MEM_COMMIT, PAGE_EXECUTE_WRITECOPY); // Check why no mem?
 
 	system("pause");
 	return 0;
